4c1418ed87b46121f797bd84fc91eaa26a64e8c6
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-02-01 19:19:28 +0545

    Added navbar component

--- eclipse/src/components/Navbar.vue
+++ eclipse/src/components/Navbar.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="hello">
-    Navbar Component
-  </div>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info" v-bind:style="primaryBackground">
+            <b-navbar-brand href="#">ESALE</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template v-slot:button-content>
+                            <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'Navbar'
-  }
+    export default {
+        name: 'Navbar',
+        data() {
+            return {
+                primaryBackground: {
+                    "background-color": "#3485d6 !important"
+                }
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
