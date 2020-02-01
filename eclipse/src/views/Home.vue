4b3c30de8396680a13960e7b8c3dac93de63ea57
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-02-01 19:19:28 +0545

    Added navbar component

--- eclipse/src/views/Home.vue
+++ eclipse/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <Navbar/>
     <Item/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '../components/Navbar.vue'
 import Item from "../components/Item"
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    Navbar,
     Item,
   }
 }
