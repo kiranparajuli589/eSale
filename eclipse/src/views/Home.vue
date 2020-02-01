8a42a8e68dca023c72b7d37e2905d8b91a088893
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-02-01 16:42:04 +0545

    Connected Vue with djangorest

--- eclipse/src/views/Home.vue
+++ eclipse/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <Item/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '../components/Navbar.vue'
+import Item from "../components/Item"
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Navbar,
+    Item,
   }
 }
 </script>
