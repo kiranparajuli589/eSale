bad936fca16729a6f783227078a278e0ee987543
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-02-03 17:06:03 +0545

    raw

--- auth_api/views.py
+++ auth_api/views.py
@@ -1,54 +1,35 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-
+import os
 import requests
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
-from .serializers import CreateUserSerializer, UserSerializer
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
-CLIENT_ID = 'iesdBuevsC9TKBF2rzJWc2s8zM4T2lX5yCXdaROQ'
-CLIENT_SECRET = 'sgOGSCBaYCSx9vvQ1DNT1jtVAvrQQYNW9yjA32Uu3qNYaP2eT1qs0epdbSa7wLAsmVO59hKXxQSwKqLgsIYxHwH8HEOLQsOv86TPPzDhjXPdADv5fna6CtYoVYDiQoW3'
-
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def register(request):
-    """
-    Registers user to the server. Input should be in the format:
-    {"username": "username", "password": "password"}
-    """
-    # Put the data from the request into the serializer
-    serializer = CreateUserSerializer(data=request.data)
-    # Validate the data
-    if serializer.is_valid():
-        # If it is valid, save the data (creates a user).
-        serializer.save()
-        # Then we get a token for the created user.
-        # This could be done differently
-        r = requests.post('http://127.0.0.1:8888/o/token/',
-                          data={
-                              'grant_type': 'password',
-                              'username': request.data['username'],
-                              'password': request.data['password'],
-                              'client_id': CLIENT_ID,
-                              'client_secret': CLIENT_SECRET,
-                          },
-                          )
-        return Response(r.json())
-    return Response(serializer.errors)
-
+def getMyUser(id):
+ resp = get_token()
+ print(resp)
+#  headers = {
+#     "Authorization": "Bearer {}".format('')
+#  }
+#  base_url = 'http://0.0.0.0:8000/auth/users/{}'.format(id)
+#  r = requests.get(
+#         base_url,
+#         headers=headers
+#      )
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def get_token(request):
-    """
+    '''
     Gets tokens with username and password. Input should be in the format:
     {"username": "username", "password": "password"}
-    """
+    '''
     r = requests.post(
-        'http://127.0.0.1:8888/o/token/',
+    'http://0.0.0.0:8000/o/token/',
         data={
             'grant_type': 'password',
             'username': request.data['username'],
@@ -63,12 +44,12 @@ def get_token(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def refresh_token(request):
-    """
+    '''
     Registers user to the server. Input should be in the format:
     {"refresh_token": "<token>"}
-    """
+    '''
     r = requests.post(
-        'http://127.0.0.1:8888/o/token/',
+    'http://0.0.0.0:8000/o/token/',
         data={
             'grant_type': 'refresh_token',
             'refresh_token': request.data['refresh_token'],
@@ -82,29 +63,20 @@ def refresh_token(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def revoke_token(request):
-    """
+    '''
     Method to revoke tokens.
     {"token": "<token>"}
-    """
+    '''
     r = requests.post(
-        'http://127.0.0.1:8888/o/token/',
+        'http://0.0.0.0:8000/o/revoke_token/',
         data={
             'token': request.data['token'],
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
         },
     )
-    # If it goes well return success message (would be empty otherwise)
+    # If it goes well return sucess message (would be empty otherwise)
     if r.status_code == requests.codes.ok:
         return Response({'message': 'token revoked'}, r.status_code)
     # Return the error if it goes badly
     return Response(r.json(), r.status_code)
-
-
-# class UsersViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('id')
-#     lookup_value_regex = '[0-9a-zA-Z]'
-#     serializer_class = UserSerializer
