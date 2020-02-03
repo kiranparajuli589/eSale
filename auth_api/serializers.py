b9eb10363af9166896c6edf99a36d747a7833352
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-02-03 17:06:03 +0545

    added oauth_2 authentication & setup user retrive api

--- auth_api/serializers.py
+++ auth_api/serializers.py
@@ -1,22 +1,33 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from django.contrib.auth.models import Group
+from rest_framework import serializers, generics, permissions
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password')
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-
+# first we define the serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("__all__")
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ("name", )
+
+# Create the API views
+class UserList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetails(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupList(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+    required_scopes = ['groups']
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- auth_api/views.py
+++ auth_api/views.py
@@ -1,110 +1,194 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-
+import json
+import os
 import requests
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from rest_framework.response import Response
 
-from .serializers import CreateUserSerializer, UserSerializer
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+ESALE_SERVER_BACKEND = 'http://' + os.getenv('ESALE_SERVER_BACKEND')
+GET_TOKEN_URL = '{}/o/token/'.format(ESALE_SERVER_BACKEND)
+last_token = ""
+last_token_response = {}
 
-CLIENT_ID = 'iesdBuevsC9TKBF2rzJWc2s8zM4T2lX5yCXdaROQ'
-CLIENT_SECRET = 'sgOGSCBaYCSx9vvQ1DNT1jtVAvrQQYNW9yjA32Uu3qNYaP2eT1qs0epdbSa7wLAsmVO59hKXxQSwKqLgsIYxHwH8HEOLQsOv86TPPzDhjXPdADv5fna6CtYoVYDiQoW3'
 
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def register(request):
+def get_present_token(request):
     """
-    Registers user to the server. Input should be in the format:
-    {"username": "username", "password": "password"}
+    returns present working token
+    :return: (str)
     """
-    # Put the data from the request into the serializer
-    serializer = CreateUserSerializer(data=request.data)
-    # Validate the data
-    if serializer.is_valid():
-        # If it is valid, save the data (creates a user).
-        serializer.save()
-        # Then we get a token for the created user.
-        # This could be done differently
-        r = requests.post('http://127.0.0.1:8888/o/token/',
-                          data={
-                              'grant_type': 'password',
-                              'username': request.data['username'],
-                              'password': request.data['password'],
-                              'client_id': CLIENT_ID,
-                              'client_secret': CLIENT_SECRET,
-                          },
-                          )
-        return Response(r.json())
-    return Response(serializer.errors)
+    global last_token
+    if not last_token:
+        admin_get_new_token(request)
+    return last_token
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def get_token(request):
+def set_token(token):
     """
-    Gets tokens with username and password. Input should be in the format:
-    {"username": "username", "password": "password"}
+    sets token as present working token
+    :param token: (str)
+    :return (void)
     """
-    r = requests.post(
-        'http://127.0.0.1:8888/o/token/',
-        data={
-            'grant_type': 'password',
-            'username': request.data['username'],
-            'password': request.data['password'],
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-        },
+    global last_token
+    last_token = token
+
+
+def set_last_token_response(token_response):
+    """
+    sets lasts token response to global var
+    :param token_response: (dict)
+    :return (dict)
+    """
+    global last_token_response
+    last_token_response = token_response
+
+
+def get_last_token_response():
+    """
+    return last token response
+    :return: (dict)
+    """
+    global last_token_response
+    return last_token_response
+
+
+def get_authorization_header(request):
+    """
+    :return: (dict)
+    """
+    token = get_present_token(request)
+    return {'Authorization': '{} {}'.format('Bearer', token)}
+
+
+def admin_get_new_token(request):
+    """
+        Gets tokens with username and password. Input should be in the format:
+        :return (void)
+    """
+
+    data = {
+        'grant_type': 'password',
+        'username': 'admin',
+        'password': 'admin',
+    }
+    auth = (CLIENT_ID, CLIENT_SECRET)
+    # sending get request and saving the response as response object
+    response = requests.post(
+        url=GET_TOKEN_URL,
+        auth=auth,
+        data=data
     )
-    return Response(r.json())
+    # extracting data in json format
+    json_response = response.json()
+    set_token(json_response['access_token'])
+    set_last_token_response(json_response)
+    return JsonResponse(json_response)
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
 def refresh_token(request):
     """
     Registers user to the server. Input should be in the format:
     {"refresh_token": "<token>"}
+    :return (void)
     """
-    r = requests.post(
-        'http://127.0.0.1:8888/o/token/',
-        data={
+    token_response = get_last_token_response()
+    data = {
             'grant_type': 'refresh_token',
-            'refresh_token': request.data['refresh_token'],
+            'refresh_token': token_response['refresh_token'],
+            'username': 'admin',
+            'password': 'password',
             'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-        },
-    )
-    return Response(r.json())
+            'client_secret': CLIENT_SECRET
+        }
+    headers = get_authorization_header(request)
+    with requests.Session() as s:
+        s.headers.update(headers)
+        response = s.post(
+            url=GET_TOKEN_URL,
+            data=data
+        )
+    json_response = response.json()
+    set_last_token_response(json_response)
+    set_token(json_response)
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
 def revoke_token(request):
     """
-    Method to revoke tokens.
+    Method to revoke/officially un-register tokens.
     {"token": "<token>"}
+    :return (void)
     """
-    r = requests.post(
-        'http://127.0.0.1:8888/o/token/',
-        data={
-            'token': request.data['token'],
+    data = {
+            'token': get_present_token(request),
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
-        },
-    )
-    # If it goes well return success message (would be empty otherwise)
-    if r.status_code == requests.codes.ok:
-        return Response({'message': 'token revoked'}, r.status_code)
+        }
+    headers = get_authorization_header(request)
+    with requests.Session() as s:
+        s.headers.update(headers)
+        response = s.post(
+            url='{}/o/revoke_token'.format(ESALE_SERVER_BACKEND),
+            data=data
+        )
+    json_response = response.json()
+    set_last_token_response(json_response)
+    set_token(json_response['access_token'])
+    # If it goes well return sucess message (would be empty otherwise)
+    if response.status_code == requests.codes.ok:
+        return Response({'message': 'token revoked'}, response.status_code)
     # Return the error if it goes badly
-    return Response(r.json(), r.status_code)
+    return Response(response.json(), response.status_code)
 
 
-# class UsersViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('id')
-#     lookup_value_regex = '[0-9a-zA-Z]'
-#     serializer_class = UserSerializer
+def get_all_users(request):
+    """
+    returns json response of all created users
+    Args:
+         request (object)
+    Returns:
+         dict: json response of get request
+    """
+    headers = get_authorization_header(request)
+    with requests.Session() as s:
+        s.headers.update(headers)
+        response = s.get(ESALE_SERVER_BACKEND+'/auth/users')
+    json_response = response.json()
+    return JsonResponse(json_response, safe=False)
+
+
+def get_a_user(request, user_id):
+    """
+    returns user with given user_id
+    Args:
+        request (object)
+        user_id: (str) user id of expected user
+    Returns:
+        dict: JsonResponse of get request
+    """
+    all_users = get_all_users(request)
+    all_users = json.loads(all_users.content)
+    found = False
+    expected_user = {}
+    for user in all_users:
+        if user["id"] == user_id:
+            found = True
+            expected_user = user
+            break
+    if not expected_user:
+        raise Exception("Expected: <user>, Found: no user with provided id: {}".format(user_id))
+    return JsonResponse(json.dumps(expected_user), safe=False)
+
+
+def authenticate_user(username, password):
+    """
+    Args:
+        username(str): username
+        password(str): password
+    Returns:
+       JsonResponse
+    """
+    test_user = authenticate(username=username, password=password)
+    print(test_user)
+    print(type(test_user))
+    return JsonResponse(test_user, safe=False)
