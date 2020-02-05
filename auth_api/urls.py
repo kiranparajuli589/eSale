5e993b1451754b349e44ca093f06f72f7978b518
Kiran Parajuli <kiranparajuli589@gmail.com>
2020-02-05 20:10:32 +0545

    working oauth api and users api

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -8,10 +8,10 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('users/', serializers.UserList.as_view()),
-    path('test/', views.get_my_user),
+    path('test/', views.get_all_users),
     path('users/<pk>', serializers.UserDetails.as_view()),
     path('groups/', serializers.GroupList.as_view()),
-    path('token/', views.get_token),
+    path('token/', views.get_new_token),
     path('token/refresh/', views.refresh_token),
     path('token/revoke/', views.revoke_token),
 ]
--- auth_api/views.py
+++ auth_api/views.py
@@ -11,14 +11,39 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 ESALE_SERVER_BACKEND = 'http://' + os.getenv('ESALE_SERVER_BACKEND')
 GET_TOKEN_URL = '{}/o/token/'.format(ESALE_SERVER_BACKEND)
+last_token = ""
+last_token_response = {}
 
 
-def get_my_user(request):
-    token_response = get_token(request)
-    print(token_response)
-    token = token_response['access_token']
-    token_type = token_response['token_type']
-    headers = {'Authorization': '{} {}'.format(token_type, token)}
+def get_present_token():
+    global last_token
+    if not last_token:
+        get_new_token()
+    return last_token
+
+
+def set_token(token):
+    global last_token
+    last_token = token
+
+
+def set_last_token_response(token_response):
+    global last_token_response
+    last_token_response = token_response
+
+
+def get_last_token_response():
+    global last_token_response
+    return last_token_response
+
+
+def get_authorization_header():
+    token = get_present_token()
+    return {'Authorization': '{} {}'.format('Bearer', token)}
+
+
+def get_all_users():
+    headers = get_authorization_header()
 
     with requests.Session() as s:
         s.headers.update(headers)
@@ -27,9 +52,7 @@ def get_my_user(request):
     return response.json()
 
 
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-def get_token(request):
+def get_new_token():
     """
         Gets tokens with username and password. Input should be in the format:
         {"username": "username", "password": "password"}
@@ -47,46 +70,59 @@ def get_token(request):
         data=data
     )
     # extracting data in json format
-    return response.json()
+    json_response = response.json()
+    set_token(json_response['access_token'])
+    set_last_token_response(json_response)
 
 
-def refresh_token(request):
+def refresh_token():
     """
     Registers user to the server. Input should be in the format:
     {"refresh_token": "<token>"}
     """
+    token_response = get_last_token_response()
     data = {
             'grant_type': 'refresh_token',
+            'refresh_token': token_response['refresh_token'],
             'username': 'admin',
             'password': 'password',
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
         }
-    # auth = (CLIENT_ID, CLIENT_SECRET)
+    headers = get_authorization_header()
+    with requests.Session() as s:
+        s.headers.update(headers)
+        response = s.post(
+            url=GET_TOKEN_URL,
+            data=data
+        )
+    json_response = response.json()
+    set_last_token_response(json_response)
+    set_token(json_response)
 
-    r = requests.post(
-        GET_TOKEN_URL,
-        auth=auth,
-        data=data
-    )
-    return Response(r.json())
 
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
 def revoke_token(request):
-    '''
+    """
     Method to revoke tokens.
     {"token": "<token>"}
-    '''
-    r = requests.post(
-        '{}/o/revoke_token/'.format(ESALE_SERVER_BACKEND),
-        data={
-            'token': request.data['token'],
+    """
+    data = {
+            'token': get_present_token(),
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
-        },
-    )
+        }
+    headers = get_authorization_header()
+    with requests.Session() as s:
+        s.headers.update(headers)
+        response = s.post(
+            url='{}/o/revoke_token'.format(ESALE_SERVER_BACKEND),
+            data=data
+        )
+    json_response = response.json()
+    set_last_token_response(json_response)
+    set_token(json_response['access_token'])
     # If it goes well return sucess message (would be empty otherwise)
-    if r.status_code == requests.codes.ok:
-        return Response({'message': 'token revoked'}, r.status_code)
+    if response.status_code == requests.codes.ok:
+        return Response({'message': 'token revoked'}, response.status_code)
     # Return the error if it goes badly
-    return Response(r.json(), r.status_code)
+    return Response(response.json(), response.status_code)
